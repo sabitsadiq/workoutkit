6681504f9707d29302b54d6ed2f688b299f56db1
sadiq <sabitsdq@gmail.com>
2024-06-20 14:18:37 +0100

    updated the signup and login routes

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
@@ -13,5 +15,43 @@ const userSchema = new Schema({
     required: true,
   },
 });
+// static signup method
+
+userSchema.statics.signup = async function (email, password) {
+  // validation
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Email is not valid");
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password not strong enogh");
+  }
+
+  const exist = await this.findOne({ email });
+  if (exist) {
+    throw Error("Email already in use");
+  }
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+  const user = await this.create({ email, password: hash });
+  return user;
+};
+// static login method
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+  const user = await this.findOne({ email });
+  if (!user) {
+    throw Error("Incorrect credentials");
+  }
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    throw Error("Incorrect credentials");
+  }
+  return user;
+};
 
 module.exports = mongoose.model("User", userSchema);
